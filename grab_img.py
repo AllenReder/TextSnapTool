11f52ab2dff489d62a19d677c7407ce52d06c1ca
Allen Reder <540809998@qq.com>
2023-04-22 10:37:15 +0800

    封装截图功能

--- grab_img.py
+++ grab_img.py
@@ -1,44 +1,49 @@
-
 import tkinter as tk
 from PIL import ImageGrab
 
-root = tk.Tk()
-root.attributes("-fullscreen", True)
-root.attributes("-alpha", 0.1)
 
-canvas = tk.Canvas(root, bg="black", highlightthickness=0)
-canvas.pack(fill=tk.BOTH, expand=True)
+def grabImage():
+    rect_start = None
+    rect_end = None
+    rect = None
 
-rect_start = None
-rect_end = None
+    root = tk.Tk()
+    root.attributes("-fullscreen", True)
+    root.attributes("-alpha", 0.1)
 
-def on_mouse_down(event):
-    global rect_start, rect
-    rect_start = (event.x, event.y)
-    rect = canvas.create_rectangle(
-        rect_start[0], rect_start[1], rect_start[0], rect_start[1], outline="white", fill="white")
+    canvas = tk.Canvas(root, bg="black", highlightthickness=0)
+    canvas.pack(fill=tk.BOTH, expand=True)
+
+    # 按下
+    def on_mouse_down(event):
+        nonlocal rect_start, rect
+        rect_start = (event.x, event.y)
+        rect = canvas.create_rectangle(
+            rect_start[0], rect_start[1], rect_start[0], rect_start[1], outline="white", fill="white")
+
+    # 移动
+    def on_mouse_move(event):
+        nonlocal rect_start, rect
+        canvas.coords(rect, rect_start[0], rect_start[1], event.x, event.y)
+
+    # 松开
+    def on_button_release(event):
+        nonlocal rect_end
+        rect_end = (event.x, event.y)
+        root.destroy()
+
+    canvas.bind("<ButtonPress-1>", on_mouse_down)
+    canvas.bind("<B1-Motion>", on_mouse_move)
+    canvas.bind("<ButtonRelease-1>", on_button_release)
+
+    root.mainloop()
+
+    grab_box = (rect_start[0], rect_start[1], rect_end[0], rect_end[1])
+    print(grab_box)
+    im = ImageGrab.grab(bbox=grab_box)
+
+    return im
 
 
-def on_mouse_move(event):
-    global rect_start, rect
-    canvas.coords(rect, rect_start[0], rect_start[1], event.x, event.y)
-
-
-def on_button_release(event):
-    global rect_end
-    rect_end = (event.x, event.y)
-    root.destroy()
-
-
-canvas.bind("<ButtonPress-1>", on_mouse_down)
-canvas.bind("<B1-Motion>", on_mouse_move)
-canvas.bind("<ButtonRelease-1>", on_button_release)
-
-root.mainloop()
-
-grab_box = (rect_start[0], rect_start[1], rect_end[0], rect_end[1])
-print(grab_box)
-im = ImageGrab.grab(bbox=grab_box)
-
-# 将截图保存为文件
-im.save("screenshot.png")
+# img = grabImage()
+# img.save("screenshot.png")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnocr import CnOcr
-img = 'C:/Users/Allen/Pictures/test.jpg'
-ocr = CnOcr()  # 所有参数都使用默认值
+# img = 'C:/Users/Allen/Pictures/test.jpg'
+# ocr = CnOcr()  # 所有参数都使用默认值
 
 
 if __name__ == '__main__':
