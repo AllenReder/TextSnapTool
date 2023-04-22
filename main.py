5462677e1c3c48c7ecd7b94d3ea8e4ef502d600d
Allen Reder <540809998@qq.com>
2023-04-22 10:41:03 +0800

    测试main中截图识别功能

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from cnocr import CnOcr
+from grab_img import grabImage
+
+
 # img = 'C:/Users/Allen/Pictures/test.jpg'
-# ocr = CnOcr()  # 所有参数都使用默认值
+ocr = CnOcr()  # 所有参数都使用默认值
 
 
 if __name__ == '__main__':
+    img = grabImage()
     out = ocr.ocr(img)
     for thing in out:
         print(thing['text'], end='')
